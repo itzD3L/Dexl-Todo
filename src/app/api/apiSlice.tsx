4bb0cb1abb6ba96f340de2be5a7da665547d1330
Marchus Deligero <marchuso.deligero@gmail.com>
2025-02-15 00:22:47 +0800

    update baseUrl

--- src/app/api/apiSlice.tsx
+++ src/app/api/apiSlice.tsx
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials, setSessionExpired } from '../../features/auth/authSlice';
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://dexlTodo-api.onrender.com',
+    baseUrl: 'https://dexl-todo-api.onrender.com',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = (getState() as { auth: { token: string } }).auth.token;
